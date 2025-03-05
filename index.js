c6067e093ec5b362bfa8ad132d29630463ca6bf1
saba00001 <164893567+saba00001@users.noreply.github.com>
2025-03-05 15:32:28 +0400

    Update index.js

--- index.js
+++ index.js
@@ -29,31 +29,19 @@ function updateStatus() {
   console.log('\x1b[33m[ STATUS ]\x1b[0m', `Updated status to: Playing HBRP`);
 }
 
-// `setTimeout`-ის წაგება BotGhost-ის სტარტის შემდეგ
 function setStatusInterval() {
   setInterval(() => {
-    updateStatus(); // განაახლეთ სტატუსი ყოველ 10 წამში
-  }, 10000);
-  
-  setTimeout(() => {
-    // ეს ბლოკი მინიმუმ 30 წამში ერთხელ აგრძელებს ფუნქციონირებას
-    setStatusInterval();
-  }, 30000);
-}
-
-function heartbeat() {
-  setInterval(() => {
-    console.log('\x1b[35m[ HEARTBEAT ]\x1b[0m', `Bot is alive at ${new Date().toLocaleTimeString()}`);
-  }, 30000);
+    updateStatus(); // განაახლე სტატუსი ყოველ 5 წამში
+  }, 5000); // 5 წამში ერთხელ
 }
 
 client.once('ready', () => {
   console.log('\x1b[36m[ INFO ]\x1b[0m', `\x1b[32mLogged in as: ${client.user.tag} ✅\x1b[0m`);
   console.log('\x1b[36m[ INFO ]\x1b[0m', `\x1b[35mBot ID: ${client.user.id} \x1b[0m`);
   console.log('\x1b[36m[ INFO ]\x1b[0m', `\x1b[34mConnected to ${client.guilds.cache.size} server(s) \x1b[0m`);
-  
+
   updateStatus();
-  setStatusInterval(); // 30 წამში ერთხელ იწყებს ფუნქციონირებას
+  setStatusInterval(); // 5 წამში ერთხელ იწყებს ფუნქციონირებას
   heartbeat();
 });
 
